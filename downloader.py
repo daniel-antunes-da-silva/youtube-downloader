d92cc967ece7c8a602ad70d37cd61733d83a6fcd
Daniel <dev.daniel.antunes@gmail.com>
2023-08-18 20:48:31 -0300

    adicionado tratamentos de erros e informativo de qualidade do arquivo.

--- downloader.py
+++ downloader.py
@@ -1,29 +1,49 @@
 from pytube import YouTube
 import os
 
-link = str(input('Digite o link do youtube: '))
-yt = YouTube(link)
-titulo_do_video = yt.title
-escolha_usuario = str(input('O que você quer baixar?\n'
-                            '[1] - MP3\n'
-                            '[2] - MP4\n'
-                            'Sua escolha: '))
-print('Obs: o arquivo será baixado no diretório atual.')
+try:
+    link = str(input('Digite o link do youtube: '))
+    yt = YouTube(link)
+    titulo_do_video = yt.title
+    escolha_usuario = str(input('O que você quer baixar?\n'
+                                '[1] - Apenas áudio (MP3)\n'
+                                '[2] - Video com áudio (MP4)\n'
+                                'Sua escolha: '))
+    print('Obs: o arquivo será baixado no diretório atual.')
 
-if escolha_usuario == '1':
-    video = yt.streams.filter(only_audio=True).first()
-    print(f'Baixando {titulo_do_video}...')
-    arquivo_em_video = video.download()
-    base, extensao = os.path.splitext(arquivo_em_video)
-    arquivo_em_audio = base + '.mp3'
-    os.rename(arquivo_em_video, arquivo_em_audio)
-    '''
-    Também poderia ser feito dessa forma, com o replace, desde que o arquivo fosse sempre em .mp4
-    arquivo_em_audio = arquivo_em_video.replace('.mp4', '.mp3')
-    os.rename(arquivo_em_video, arquivo_em_audio)
-    '''
-elif escolha_usuario == '2':
-    stream = yt.streams.get_highest_resolution()
-    print(f'Baixando {titulo_do_video}...')
-    print(stream)
-    stream.download()
+    if escolha_usuario == '1':
+        try:
+            video = yt.streams.get_audio_only()
+            print(f'Baixando áudio: {titulo_do_video}...')
+            print(f'Qualidade: ', video.abr)
+            arquivo_em_video = video.download()
+            base, extensao = os.path.splitext(arquivo_em_video)
+            arquivo_em_audio = base + '.mp3'
+            os.rename(arquivo_em_video, arquivo_em_audio)
+        except FileExistsError:
+            print('O arquivo já existe no diretório, portanto não foi baixado.')
+        except:
+            print('Ocorreu algum erro.')
+        else:
+            print('Arquivo baixado com êxito.')
+        '''
+        Também poderia ser feito dessa forma, com o replace, desde que o arquivo fosse sempre em .mp4
+        arquivo_em_audio = arquivo_em_video.replace('.mp4', '.mp3')
+        os.rename(arquivo_em_video, arquivo_em_audio)
+        '''
+    elif escolha_usuario == '2':
+        try:
+            stream = yt.streams.get_highest_resolution()
+            print(f'Baixando vídeo: {titulo_do_video}...')
+            print(f'Qualidade: {stream.resolution} | {stream.fps}fps')
+            stream.download()
+        except FileExistsError:
+            print('O arquivo já existe no diretório, portanto não foi baixado.')
+        except:
+            print('Ocorreu algum erro.')
+        else:
+            print('Arquivo baixado com êxito.')
+    else:
+        print('Opção inválida. Execute o programa novamente para baixar áudio ou video do youtube.')
+except:
+    print('Ops, talvez o link esteja quebrado ou incorreto. Verifique, por favor!')
